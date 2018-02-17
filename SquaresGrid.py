e6ade6362804bdb909a34220c04a865a8d2216bd
Alex Kiral <alexkiral@gmail.com>
2018-02-17 15:51:10 -0600

    Created an update function that assumes you can get the distance between two GridSquares as a vector

--- SquaresGrid.py
+++ SquaresGrid.py
@@ -52,3 +52,52 @@ def color_edge_initialize(img):
 
 def non_color_edge_initialize(img):
 	return None
+
+def update_position(previous_squares, current_squares, previous_position):
+
+	#Delete all of this. Switch to minimizing cost function, where cost function is the distance between squares
+
+	#I first go through all vectors between squares and find the minumum from each of the previous squares
+	min_distances_vectors = []
+
+	for ps in previous_squares:
+		min_dist_vec = 0
+		min_cs = None
+
+		#I am assuming square does not have the same distance from 2 previous squares
+		for cs in current_squares:
+			dist_vector = compare_two_squares(ps, cs)
+			min_temp = min(min_dist, np.linalg.norm(dist_vector))
+
+			if not min_temp == np.linalg.norm(min_dist_vec):
+				min_dist_vec = dist_vector
+				min_cs = cs
+
+		min_distances_vectors.append(min_dist_vec)
+
+	#If there are different numbers of squares previous_squares and current_squares, I have to get rid of all the extra
+	difference = min(0, previous_squares.length - current_squares.length)
+
+	for i in range(difference):
+		max_item = None
+		for dist_tuple in min_distances_vectors:
+			if max_item is None or np.linalg.norm(dist_tuple[0]) > np.linalg.norm(max_item[0]):
+				max_item = dist_tuple
+
+		if max_item is not None:
+			min_distances_vectors.remove(max_item)
+
+	avg_dist_vector = []
+	for dist_tuple in min_distances_vectors:
+		for i in range(avg_dist_vector.length):
+			avg_dist_vector[i] += dist_tuple[i]
+
+	for i in range(avg_dist_vector.length):
+		avg_dist_vector[i] /= min_distances_vectors.length
+
+	return avg_dist_vector
+
+# return vector distance between centers of squares
+def compare_two_squares(s_1, s_2):
+	return [1,1]
+
