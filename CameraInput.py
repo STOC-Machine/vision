4db1916a27141467024c8fa50e29b49e85a1e587
Kelsey Krantz <krantz77@gmail.com>
2018-04-14 14:21:38 -0500

    Version of grid dection using Canny and Houghlines

--- CameraInput.py
+++ CameraInput.py
@@ -2,129 +2,208 @@ import cv2
 import sys
 import numpy as np
 import glob
-import GridSquares as grid
-import color_detect as cd
+import grid_squares as grid
+import vector as vec
 
-#Alex's sysout is broken, so he uses a log file instead of print()
-import logging
-logging.basicConfig(filename='birdsview.log', level=logging.DEBUG)
+FONT = cv2.FONT_HERSHEY_SIMPLEX
 
-#CameraMatrix=np.array([[376.60631072, 0., 334.94985263], [0., 376.37590044, 245.47987032], [0., 0., 1.]])
-CameraMatrix=np.array([[811.75165344, 0., 317.03949866],[0., 811.51686214, 247.65442989],[0., 0., 1.]]) # Logitech values found in CalibrationValues.txt
-#distortionCoefficients=np.array([-3.30211385e-01, 1.58724644e-01, -1.87573090e-04, 4.55691783e-04, -4.98096761e-02])
-distortionCoefficients=np.array([-3.00959078e-02, -2.22274786e-01, -5.31335928e-04, -3.74777371e-04, 1.80515550e+00]) #Logitech values found in Calibration Values.txt
-distortionCoefficients=distortionCoefficients.reshape(5,1) #Needs to be this shape
+#camera_matrix = np.array([[376.60631072, 0., 334.94985263], [0., 376.37590044, 245.47987032], [0., 0., 1.]])
+#distortion_coefficients = np.array([-3.30211385e-01, 1.58724644e-01, -1.87573090e-04, 4.55691783e-04, -4.98096761e-02])
+
+# Logitech values
+camera_matrix = np.array([[811.75165344, 0, 317.03949866],
+                          [0, 811.51686214, 247.65442989],
+                          [0, 0, 1]])
+distortion_coefficients = np.array([-3.00959078e-02, -2.22274786e-01,
+                                    -5.31335928e-04, -3.74777371e-04,
+                                    1.80515550e+00])
+distortion_coefficients = distortion_coefficients.reshape(5, 1)
 
 
-font = cv2.FONT_HERSHEY_SIMPLEX #Used for drawing text.
-camera=0 #Will be used to test camera loading
-if(len(sys.argv)<2): #If no arguments passed
-	camera=cv2.VideoCapture(0) #Load the webcam
-	filenames=[] #Don't give any filenames
-else:
-	filenames=glob.glob(sys.argv[1]) #Get the filenames from the command
-	print(filenames) #Print them, 'cause why not?
-exit=0 #Don't stop running yet
+def find_grid(img, cam_rot_guess):
+    """
+    Takes an undistorted image and a camera rotation guess and finds the position
+    of the camera relative to the grid, as well as identifying all visible
+    squares in the grid.
+    :param img: The image to process
+    :param cam_rot_guess:
+    :return: (squares, cam_rot, base_object_points)
+    """
 
-BestCamRotGuess=0
-while(len(filenames)>0 or not exit): #If there are more files, or we haven't quit yet
-	if(len(filenames)>0): #If we're running purely on files
-		exit=1 #Make it quit when we're done with files
-		filename=filenames.pop(0) #And get the first file in the list
-		try: 
-			img=cv2.imread(filename) #Read the image
-		except:
-			continue #Unless you can't. Then skip it.
-	elif(camera): #If using webcam
-		ret, img = camera.read() #Read from webcam
-	else: #If things are weird, just quit
-		break
-	if(type(img) is not np.ndarray): #Do make sure that there's an image
-		break
-	img=cv2.undistort(img,CameraMatrix,distortionCoefficients)
-	outimg=np.copy(img) #Copy the image. Not really needed, but can be nice long term
-	logging.info(cd.color_detect(img,40,80))
-	#print img.shape
-	birdsview=np.zeros([1000,1000,3],dtype=np.uint8)
-	cv2.circle(birdsview,(int(birdsview.shape[0]/2),int(birdsview.shape[1]/2)),5,(255,255,0),-1)
-	squares,BestCamRotGuess=grid.getSquareStats(img,CameraMatrix,np.array([[]]),BestCamRotGuess) #It's a magic function! Yay!
-	#print(contour,len(squares)) #Print the # of squares found
-	#print(len(img),len(img[0]))
-	gluedSquareCorners=[]
-	gluedSquareCoords=[]
-	squarelength=28.5
-	squareGap=2
-	baseobjectpoints=[[-squarelength/2,-squarelength/2,0],[-squarelength/2,squarelength/2,0],[squarelength/2,squarelength/2,0],[squarelength/2,-squarelength/2,0]]
-	if(BestCamRotGuess!=0):
-		tempcamline=grid.vecadd(grid.scalarmult(BestCamRotGuess,50),[birdsview.shape[0]/2,birdsview.shape[0]/2,0])
-		cv2.line(birdsview,(int(tempcamline[0]),int(tempcamline[1])),(int(birdsview.shape[0]/2),int(birdsview.shape[1]/2)),(0,0,255),1,cv2.LINE_AA)
-	for square in squares:
-		tempvec=grid.vecsub(square.location,squares[0].location)
-		INeedAnIndex=0
-		while(INeedAnIndex<4):
-			tempdrawvec=grid.vecadd(square.location,baseobjectpoints[INeedAnIndex])
-			tempdrawvec2=grid.vecadd(square.location,baseobjectpoints[INeedAnIndex-1])
-			INeedAnIndex+=1
-			cv2.line(birdsview,(int(tempdrawvec[0]+birdsview.shape[0]/2),int(tempdrawvec[1]+birdsview.shape[1]/2)),(int(tempdrawvec2[0]+birdsview.shape[0]/2),int(tempdrawvec2[1]+birdsview.shape[1]/2)),(255,255,255),3,cv2.LINE_AA)
-		tempvec[0]=(squarelength+squareGap)*round(tempvec[0]/(squarelength+squareGap),0)
-		tempvec[1]=(squarelength+squareGap)*round(tempvec[1]/(squarelength+squareGap),0)
-		tempvec[2]=0
-		for i in baseobjectpoints:
-			gluedSquareCoords.append([[grid.vecadd(i,tempvec)[0]],[grid.vecadd(i,tempvec)[1]],[grid.vecadd(i,tempvec)[2]]])
-		for i in grid.denumpify(square.corners):
-			gluedSquareCorners.append([[i[0]],[i[1]]])
-		#print tempvec
-		#print(square.location)
-		x=0
-		y=0
-		for point in square.corners:
-			x+=point[0][0]
-			y+=point[0][1]
-		x=int(x/4)
-		y=int(y/4)
-		cv2.putText(img,str(int(abs(square.location[2])))+" "+str(int(square.score*100)),(x,y), font, 1,(255,255,255),1,cv2.LINE_AA)
+    birds_view = np.zeros([1000, 1000, 3], dtype=np.uint8)
+    cv2.circle(birds_view,
+               (int(birds_view.shape[0] / 2), int(birds_view.shape[1] / 2)), 5,
+               (255, 255, 0), -1)
+    squares, cam_rot_guess = grid.get_square_stats(img, camera_matrix,
+                                                   np.array([[]]),
+                                                   cam_rot_guess)
 
-		cv2.polylines(img,[square.corners],True,(255,0,0)) #Draw both squares
-		#print square.location
-		#print square.side1
-		cv2.drawContours(img,square.contour,True,(0,255,0))
-		
-		#print(len([square for square in squares if square.score > scorethreshold]))
-	if(len(squares)>0):
-		gluedSquareCorners=np.asarray(gluedSquareCorners).astype(float)
-		gluedSquareCoords=np.asarray(gluedSquareCoords).astype(float)
-		gluedSquareCorners.reshape(len(gluedSquareCorners),2,1)
-		gluedSquareCoords.reshape(len(gluedSquareCoords),3,1)
-		#for square2 in squares:
-			#print square2.corners
-		#for square2 in squares:
-			#print grid.vecsub(square2.location,squares[0].location)
-		#print gluedSquareCorners
-		#print gluedSquareCoords
-		inliers,fullrvec,fulltvec=cv2.solvePnP(gluedSquareCoords,gluedSquareCorners,CameraMatrix,distortionCoefficients,squares[0].rvec,squares[0].tvec,True) #Where the magic happens. Turns gets vector from camera to center of square
-		#print fulltvec,fullrvec
-		rotMatrix=cv2.Rodrigues(fullrvec)
-		camerapos=np.multiply(cv2.transpose(rotMatrix[0]), -1).dot(fulltvec)
-		print(camerapos)
-		print(squares[0].location)
-		camToGridTransform=np.concatenate((cv2.transpose(rotMatrix[0]),camerapos),axis=1)
-		gridToCamTransform=np.linalg.inv(np.concatenate((camToGridTransform,np.array([[0,0,0,1]])),axis=0))
-		camRot=list(camToGridTransform.dot(np.array([0,0,1,0])))
-		tempcamline2=grid.vecadd(grid.scalarmult(camRot,50),[birdsview.shape[0]/2,birdsview.shape[0]/2,0])
-		cv2.line(birdsview,(int(tempcamline2[0]),int(tempcamline2[1])),(int(birdsview.shape[0]/2),int(birdsview.shape[1]/2)),(255,0,255),1,cv2.LINE_AA)
-	print("") #Divider line
-	try:
-		cv2.imshow("hi",img) #This is mainly to let my borked python3 install, which can't display images, work.
-		cv2.imshow("Birds eye view",birdsview)
-		if(camera): #If we're doing video
-			if(len(squares)>0):
-				BestCamRotGuess=squares[0].camRot
-			if cv2.waitKey(1) & 0xFF == ord('q'): #Let the program run while waiting for q to be pressed
-				break #Exit
-		else: #If it's just files,
-			BestCamRotGuess=0
-			cv2.waitKey(0) #Wait for a key to continue on
-			cv2.destroyAllWindows() #And remove the old window
-	except:
-		pass
-#cv2.destroyAllWindows() #And don't leave silly windows behind.
+    cam_rot = cam_rot_guess
+
+    glued_square_corners = []
+    glued_square_coords = []
+    square_length = 28.5
+    square_gap = 2
+    base_object_points = [[-square_length / 2, -square_length / 2, 0],
+                          [-square_length / 2, square_length / 2, 0],
+                          [square_length / 2, square_length / 2, 0],
+                          [square_length / 2, -square_length / 2, 0]]
+
+    for square in squares:
+        temp_vec = vec.sub(square.location, squares[0].location)
+
+        temp_vec[0] = (square_length + square_gap) * round(
+            temp_vec[0] / (square_length + square_gap), 0)
+        temp_vec[1] = (square_length + square_gap) * round(
+            temp_vec[1] / (square_length + square_gap), 0)
+        temp_vec[2] = 0
+
+        for i in base_object_points:
+            glued_square_coords.append(
+                [[vec.add(i, temp_vec)[0]], [vec.add(i, temp_vec)[1]],
+                 [vec.add(i, temp_vec)[2]]])
+
+        for i in vec.denumpify(square.corners):
+            glued_square_corners.append([[i[0]], [i[1]]])
+
+    if len(squares) > 0:
+        glued_square_corners = np.asarray(glued_square_corners).astype(float)
+        glued_square_coords = np.asarray(glued_square_coords).astype(float)
+        glued_square_corners.reshape(len(glued_square_corners), 2, 1)
+        glued_square_coords.reshape(len(glued_square_coords), 3, 1)
+
+        # Where the magic happens. Gets vector from camera to center of square
+        inliers, full_r_vec, full_t_vec = cv2.solvePnP(glued_square_coords,
+                                                       glued_square_corners,
+                                                       camera_matrix,
+                                                       distortion_coefficients,
+                                                       squares[0].rvec,
+                                                       squares[0].tvec, True)
+
+        rot_matrix = cv2.Rodrigues(full_r_vec)
+        camera_pos = np.multiply(cv2.transpose(rot_matrix[0]), -1).dot(
+            full_t_vec)
+
+        cam_to_grid_transform = np.concatenate(
+            (cv2.transpose(rot_matrix[0]), camera_pos), axis=1)
+
+        cam_rot = list(cam_to_grid_transform.dot(np.array([0, 0, 1, 0])))
+
+    return squares, cam_rot, base_object_points
+
+
+def display_grid(img, squares, cam_rot, base_object_points):
+    """
+    Display the output of find_grid in a pretty way.
+    :param img: the image in which the grid was found
+    :param squares: the squares in the image
+    :param cam_rot: the deduced camera orientation
+    :param base_object_points: ??
+    :return: nothing
+    """
+    birds_view = np.zeros([1000, 1000, 3], dtype=np.uint8)
+    cv2.circle(birds_view,
+               (int(birds_view.shape[0] / 2), int(birds_view.shape[1] / 2)), 5,
+               (255, 255, 0), -1)
+
+    if cam_rot != 0:
+        cam_line = vec.add(vec.scalar_mult(cam_rot, 50),
+                                [birds_view.shape[0] / 2,
+                                 birds_view.shape[0] / 2, 0])
+
+        cv2.line(birds_view, (int(cam_line[0]), int(cam_line[1])),
+                 (int(birds_view.shape[0] / 2), int(birds_view.shape[1] / 2)),
+                 (0, 0, 255), 1, cv2.LINE_AA)
+
+    for square in squares:
+        for edge_index in range(4):
+            temp_draw_vec = vec.add(square.location,
+                                         base_object_points[edge_index])
+            temp_draw_vec2 = vec.add(square.location,
+                                          base_object_points[edge_index - 1])
+
+            cv2.line(birds_view, (int(temp_draw_vec[0] + birds_view.shape[0] / 2),
+                                  int(temp_draw_vec[1] + birds_view.shape[1] / 2)),
+                     (int(temp_draw_vec2[0] + birds_view.shape[0] / 2),
+                      int(temp_draw_vec2[1] + birds_view.shape[1] / 2)),
+                     (255, 255, 255), 3, cv2.LINE_AA)
+
+        x = sum(point[0][0] for point in square.corners) // 4
+        y = sum(point[0][1] for point in square.corners) // 4
+
+        cv2.putText(img, '{} {}'.format(int(abs(square.location[2])),
+                                        int(square.score * 100)), (x, y), FONT,
+                    1, (255, 255, 255), 1, cv2.LINE_AA)
+
+        cv2.polylines(img, [square.corners], True, (255, 0, 0))
+        cv2.drawContours(img, square.contour, True, (0, 255, 0))
+
+    if len(squares) > 0:
+        cam_line2 = vec.add(vec.scalar_mult(cam_rot, 50),
+                            [birds_view.shape[0] / 2,
+                             birds_view.shape[0] / 2, 0])
+
+        cv2.line(birds_view, (int(cam_line2[0]), int(cam_line2[1])),
+                 (int(birds_view.shape[0] / 2), int(birds_view.shape[1] / 2)),
+                 (255, 0, 255), 1, cv2.LINE_AA)
+
+    cv2.imshow("Squares", img)
+    cv2.imshow("Birds eye view", birds_view)
+
+
+def run_from_camera(camera):
+    """
+    Run bird's view from camera input.
+    :param camera: the cv2 camera file handle
+    :return: nothing
+    """
+
+    cam_rot_guess = 0
+
+    # Run until q is pressed
+    while cv2.waitKey(1) & 0xFF != ord('q'):
+        ret, img = camera.read()
+
+        if type(img) is not np.ndarray:
+            print('Error: image did not read properly, skipping')
+            continue
+
+        img = cv2.undistort(img, camera_matrix, distortion_coefficients)
+        squares, cam_rot, base_object_points = find_grid(img, cam_rot_guess)
+        display_grid(img, squares, cam_rot, base_object_points)
+
+        if len(squares) > 0:
+            cam_rot_guess = squares[0].cam_rot
+
+
+def run_from_files(files):
+    """
+    Run bird's view from file input.
+    :param files: a directory/filename with wildcards
+    :return: nothing
+    """
+    files = glob.glob(files)
+
+    while len(files) > 0:
+        file = files.pop(0)
+        img = cv2.imread()
+
+        if img is None:
+            print('Error: could not read image file {}, skipping.'.format(file))
+            continue
+
+        img = cv2.undistort(img, camera_matrix, distortion_coefficients)
+        squares, cam_rot, base_object_points = find_grid(img, 0)
+        display_grid(img, squares, cam_rot, base_object_points)
+
+        # Wait for keypress to continue, close old windows
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        # Use /dev/video0 by default. Change from 0 if using different /video
+        run_from_camera(cv2.VideoCapture(0))
+    else:
+        run_from_files(sys.argv[1])
--- htransform.py
+++ htransform.py
@@ -14,34 +14,43 @@ filenames=glob.glob(sys.argv[1])
 exit=0 #Don't stop running yet
 def makeHoughLines(img):
   # print('asdfasdf')
-  red,green,blue=cv2.split(img) #split the image into components.
-  testgray=np.minimum(blue,red) #Create a new image with the minimum of b and r channels
-  testgray=np.minimum(testgray,green) #Create a new image with minimum of all three channels
-  ret,out=cv2.threshold(testgray,120,255,cv2.THRESH_BINARY_INV)
+  #red,green,blue=cv2.split(img) #split the image into components.
+  #testgray=np.minimum(blue,red) #Create a new image with the minimum of b and r channels
+  #testgray=np.minimum(testgray,green) #Create a new image with minimum of all three channels
+  print(img.dtype)
+  print(img.shape)
+  cv2.imshow("before", img)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+  #3ret,img=cv2.threshold(img,120,255,cv2.THRESH_BINARY_INV)
+  print(img.dtype)
+  print(img.shape)
+
   # print(out[0][0])
   #try:
   #print(ret)
   i=0
   j=0
   z=0
-  #cv2.imshow('Threshold',out) #Display the thresholded image
 
-  cv2.imshow('Threshold',out) #Display the thresholded image
+  #cv2.imshow('Threshold',out) #Display the thresholded image
 
 
   # cv2.waitKey(0)
   # cv2.destroyAllWindows()
   #except:
   #  pass
-  lines = cv2.HoughLinesP(out, 1, np.pi/180, 100, 100, 103)
+  lines = cv2.HoughLinesP(img, 1, np.pi/180, 25, 75, 10)
+  #lines = cv2.HoughLinesP(img, 1, np.pi/180, 200, 200, 100)
+
   #lines = cv2.cornerHarris(out, out, ksize, k[, dst[, borderType]])
   if lines == None:
     return []
   for line in lines:
           x1, y1, x2, y2 = line[0]
-          cv2.line(out, (x1,y1), (x2,y2), 160, 2)
-  cv2.imwrite('houghlines.jpg', out)
-  cv2.imshow('houghlines.jpg', out)
+          cv2.line(img, (x1,y1), (x2,y2), 160, 10)
+  cv2.imwrite('houghlines.jpg', img)
+  cv2.imshow('houghlines.jpg', img)
   # print(lines)
   cv2.waitKey(0)
   cv2.destroyAllWindows()
@@ -51,6 +60,84 @@ def makeHoughLines(img):
 
   #except:
     #pass 
+#following the sodoku website 
+#http://aishack.in/tutorials/sudoku-grabber-opencv-detection/
+def grid_detect(img):
+  red,green,blue=cv2.split(img) #split the image into components.
+  testgray=np.minimum(blue,red) #Create a new image with the minimum of b and r channels
+  testgray=np.minimum(testgray,green) #Create a new image with minimum of all three channels
+  ret,out=cv2.threshold(testgray,120,255,cv2.THRESH_BINARY_INV)
+  sudoku = out
+  outerBox = np.zeros((256, 256, 1), dtype = "uint8")
+  kernel = np.zeros((3,3,1), dtype = "uint8")
+  #kernel(0,0) = 0; 
+  sudoku = cv2.GaussianBlur(sudoku,(11,11),0)
+  kernel = np.ones((5,5),np.uint8)
+  dilation = cv2.dilate(sudoku,kernel,iterations = 1)
+  dilation = cv2.Canny(dilation,100,200)
+  cv2.imshow("dilation.jpg", dilation)
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
+  return dilation
+  
+
+  '''blob detection attempt '''
+
+  # # Set up the detector with default parameters.
+  # detector = cv2.SimpleBlobDetector_create()
+   
+  # # Detect blobs.
+  # keypoints = detector.detect(dilation)
+   
+  # # Draw detected blobs as red circles.
+  # # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+  # im_with_keypoints = cv2.drawKeypoints(dilation, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+   
+  # # Show keypoints
+  # cv2.imshow("Keypoints", im_with_keypoints)
+  # cv2.waitKey(0)
+
+
+'''blob detection attempt '''
+
+
+  # # Setup SimpleBlobDetector parameters.
+  # params = cv2.SimpleBlobDetector_Params()
+  # # Change thresholds
+  # params.minThreshold = 10
+  # params.maxThreshold = 200
+  # # Filter by Area.
+  # params.filterByArea = True
+  # params.minArea = 1500
+  # # Filter by Circularity
+  # params.filterByCircularity = True
+  # params.minCircularity = 0.1
+  # # Filter by Convexity
+  # params.filterByConvexity = True
+  # params.minConvexity = 0.87
+  # # Filter by Inertia
+  # params.filterByInertia = True
+  # params.minInertiaRatio = 0.01
+  # # Create a detector with the parameters
+  # detector = cv2.SimpleBlobDetector(params)
+  # # Detect blobs.
+  # keypoints = detector.detect(im)
+  # # Draw detected blobs as red circles.
+  # # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures
+  # # the size of the circle corresponds to the size of blob
+  # im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+  # # Show blobs
+  # cv2.imshow("Keypoints", im_with_keypoints)
+  # cv2.waitKey(0)
+
+
+  # cv2.imshow("dilation.jpg", dilation)
+  # cv2.waitKey(0)
+  # cv2.destroyAllWindows()
+  # return dilation
+
+
+
 
 BestCamRotGuess=0
 while(len(filenames)>0 or not exit): #If there are more files, or we haven't quit yet
@@ -58,16 +145,17 @@ while(len(filenames)>0 or not exit): #If there are more files, or we haven't qui
       exit=1 #Make it quit when we're done with files
       filename=filenames.pop(0) #And get the first file in the list
       #try: 
-      img=cv2.imread(filename) #Read the image
-      makeHoughLines(img)
+      img=cv2.imread(filename, cv2.IMREAD_COLOR) #Read the image
+      print(img)
+      makeHoughLines(grid_detect(img))
+      #grid_detect(img)
         #print('asdfasdf')
       #except:
         #print(';lkj;lkj;lkj')
         #continue #Unless you can't. Then skip it.
   else: #If things are weird, just quit
-      break
+    break
   if(img==None): #Do make sure that there's an image
-    break33
-  img=cv2.undistort(img,CameraMatrix,distortionCoefficients)
-
+    break 
+  img = cv2.undistort(img,CameraMatrix,distortionCoefficients)
 
