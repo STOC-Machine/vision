f605a9e2c514537c5e344bbad74a7ad325598a63
grant <grant.lee.2017@gmail.com>
2018-04-21 16:50:36 -0500

    Added orient_squares.py

---
+++ orient_squares.py
@@ -0,0 +1,192 @@
+import cv2
+import sys
+import numpy as np
+import glob
+import grid_squares as grid
+import vector as vec
+import squares_grid as sqrgrd
+
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+
+#camera_matrix = np.array([[376.60631072, 0., 334.94985263], [0., 376.37590044, 245.47987032], [0., 0., 1.]])
+#distortion_coefficients = np.array([-3.30211385e-01, 1.58724644e-01, -1.87573090e-04, 4.55691783e-04, -4.98096761e-02])
+
+# Logitech values
+camera_matrix = np.array([[811.75165344, 0, 317.03949866],
+                          [0, 811.51686214, 247.65442989],
+                          [0, 0, 1]])
+distortion_coefficients = np.array([-3.00959078e-02, -2.22274786e-01,
+                                    -5.31335928e-04, -3.74777371e-04,
+                                    1.80515550e+00])
+distortion_coefficients = distortion_coefficients.reshape(5, 1)
+
+
+def find_grid(img, cam_rot_guess):
+    """
+    Takes an undistorted image and a camera rotation guess and finds the position
+    of the camera relative to the grid, as well as identifying all visible
+    squares in the grid.
+    :param img: The image to process
+    :param cam_rot_guess:
+    :return: (squares, cam_rot, base_object_points)
+    """
+
+    birds_view = np.zeros([1000, 1000, 3], dtype=np.uint8)
+    cv2.circle(birds_view,
+               (int(birds_view.shape[0] / 2), int(birds_view.shape[1] / 2)), 5,
+               (255, 255, 0), -1)
+    squares, cam_rot_guess = grid.get_square_stats(img, camera_matrix,
+                                                   np.array([[]]),
+                                                   cam_rot_guess)
+
+    cam_rot = cam_rot_guess
+
+    glued_square_corners = []
+    glued_square_coords = []
+    square_length = 28.5
+    square_gap = 2
+    base_object_points = [[-square_length / 2, -square_length / 2, 0],
+                          [-square_length / 2, square_length / 2, 0],
+                          [square_length / 2, square_length / 2, 0],
+                          [square_length / 2, -square_length / 2, 0]]
+
+    for square in squares:
+        temp_vec = vec.sub(square.location, squares[0].location)
+
+        temp_vec[0] = (square_length + square_gap) * round(
+            temp_vec[0] / (square_length + square_gap), 0)
+        temp_vec[1] = (square_length + square_gap) * round(
+            temp_vec[1] / (square_length + square_gap), 0)
+        temp_vec[2] = 0
+
+        for i in base_object_points:
+            glued_square_coords.append(
+                [[vec.add(i, temp_vec)[0]], [vec.add(i, temp_vec)[1]],
+                 [vec.add(i, temp_vec)[2]]])
+
+        for i in vec.denumpify(square.corners):
+            glued_square_corners.append([[i[0]], [i[1]]])
+
+    if len(squares) > 0:
+        glued_square_corners = np.asarray(glued_square_corners).astype(float)
+        glued_square_coords = np.asarray(glued_square_coords).astype(float)
+        glued_square_corners.reshape(len(glued_square_corners), 2, 1)
+        glued_square_coords.reshape(len(glued_square_coords), 3, 1)
+
+        # Where the magic happens. Gets vector from camera to center of square
+        inliers, full_r_vec, full_t_vec = cv2.solvePnP(glued_square_coords,
+                                                       glued_square_corners,
+                                                       camera_matrix,
+                                                       distortion_coefficients,
+                                                       squares[0].rvec,
+                                                       squares[0].tvec, True)
+
+        rot_matrix = cv2.Rodrigues(full_r_vec)
+        camera_pos = np.multiply(cv2.transpose(rot_matrix[0]), -1).dot(
+            full_t_vec)
+
+        cam_to_grid_transform = np.concatenate(
+            (cv2.transpose(rot_matrix[0]), camera_pos), axis=1)
+
+        cam_rot = list(cam_to_grid_transform.dot(np.array([0, 0, 1, 0])))
+
+    return squares, cam_rot, base_object_points
+
+
+def display_grid(img, squares, cam_rot, base_object_points):
+    """
+    Display the output of find_grid in a pretty way.
+    :param img: the image in which the grid was found
+    :param squares: the squares in the image
+    :param cam_rot: the deduced camera orientation
+    :param base_object_points: ??
+    :return: nothing
+    """
+    birds_view = np.zeros([1000, 1000, 3], dtype=np.uint8)
+    cv2.circle(birds_view,
+               (int(birds_view.shape[0] / 2), int(birds_view.shape[1] / 2)), 5,
+               (255, 255, 0), -1)
+
+    if cam_rot != 0:
+        cam_line = vec.add(vec.scalar_mult(cam_rot, 50),
+                                [birds_view.shape[0] / 2,
+                                 birds_view.shape[0] / 2, 0])
+
+        cv2.line(birds_view, (int(cam_line[0]), int(cam_line[1])),
+                 (int(birds_view.shape[0] / 2), int(birds_view.shape[1] / 2)),
+                 (0, 0, 255), 1, cv2.LINE_AA)
+
+    for square in squares:
+        for edge_index in range(4):
+            temp_draw_vec = vec.add(square.location,
+                                         base_object_points[edge_index])
+            temp_draw_vec2 = vec.add(square.location,
+                                          base_object_points[edge_index - 1])
+
+            cv2.line(birds_view, (int(temp_draw_vec[0] + birds_view.shape[0] / 2),
+                                  int(temp_draw_vec[1] + birds_view.shape[1] / 2)),
+                     (int(temp_draw_vec2[0] + birds_view.shape[0] / 2),
+                      int(temp_draw_vec2[1] + birds_view.shape[1] / 2)),
+                     (255, 255, 255), 3, cv2.LINE_AA)
+
+        x = sum(point[0][0] for point in square.corners) // 4
+        y = sum(point[0][1] for point in square.corners) // 4
+
+        cv2.putText(img, '{} {}'.format(int(abs(square.location[2])),
+                                        int(square.score * 100)), (x, y), FONT,
+                    1, (255, 255, 255), 1, cv2.LINE_AA)
+
+        cv2.polylines(img, [square.corners], True, (255, 0, 0))
+        cv2.drawContours(img, square.contour, True, (0, 255, 0))
+
+    if len(squares) > 0:
+        cam_line2 = vec.add(vec.scalar_mult(cam_rot, 50),
+                            [birds_view.shape[0] / 2,
+                             birds_view.shape[0] / 2, 0])
+
+        cv2.line(birds_view, (int(cam_line2[0]), int(cam_line2[1])),
+                 (int(birds_view.shape[0] / 2), int(birds_view.shape[1] / 2)),
+                 (255, 0, 255), 1, cv2.LINE_AA)
+
+    cv2.imshow("Squares", img)
+    cv2.imshow("Birds eye view", birds_view)
+
+
+def run_from_camera(camera):
+    """
+    Run bird's view from camera input.
+    :param camera: the cv2 camera file handle
+    :return: nothing
+    """
+
+    position = [0.0]
+    
+    prev_squares = None 
+
+    cam_rot_guess = 0
+
+    # Run until q is pressed
+    while cv2.waitKey(1) & 0xFF != ord('q'):
+        ret, img = camera.read()
+
+        if type(img) is not np.ndarray:
+            print('Error: image did not read properly, skipping')
+            continue
+
+        img = cv2.undistort(img, camera_matrix, distortion_coefficients)
+        squares, cam_rot, base_object_points = find_grid(img, cam_rot_guess)
+        display_grid(img, squares, cam_rot, base_object_points)
+        
+        if prev_squares is not None:
+            position = sqrgrd.update_position(prev_squares, squares, position) 
+            print(position)
+            
+        prev_squares = squares 
+        
+        if len(squares) > 0:
+            cam_rot_guess = squares[0].cam_rot
+
+if __name__ == '__main__':
+    # Use /dev/video0 by default. Change from 0 if using different /video
+    run_from_camera(cv2.VideoCapture(1))
+
--- squares_grid.py
+++ squares_grid.py
@@ -57,34 +57,40 @@ def update_position(previous_squares, current_squares, previous_position):
 	matchings, flipped = frame_matching(previous_squares, current_squares)
 	matchings = remove_bad_matchings(matchings)
 
+
 	if not flipped:
 		vector_dist = vector_dist_between_frames(previous_squares, current_squares, matchings)
 	else:
 		vector_dist = vector_dist_between_frames(current_squares, previous_squares, matchings)
 
-	return previous_position + vector_dist
+	return np.array(previous_position) + np.array(vector_dist)
 
 def vector_dist_between_frames(previous_squares, current_squares, matchings):
-	vector_sum = [0,0]
-	sum_divisor = 0
+        #in the future distinguish between no matchings and distance of 0
+        if(len(matchings) == 0):
+            return [0,0]
+
+	vector_list = []
 	
 	for ps in previous_squares:
-		matched_square_name = matchings[ps.name]
-
-		if matched_square_name == None:
-			continue
+                if ps.name in matchings:
+		        matched_square_name = matchings[ps.name][0]
+                else:
+                        continue
 
 		for cs in current_squares:
 			if matched_square_name == cs.name:
-				vector_sum = vector_sum + vec_distance(ps, cs)
-				sum_divisor = sum_divisor + 1
+				vector_list.append(vec_distance(ps, cs))
+        vector_sum = np.zeros((1,2))
+        for vector in vector_list:
+            vector_sum += vector
+        return vector_sum / len(vector_list)
 
-	return (1/sum_divisor) * vector_sum
 
 def remove_bad_matchings(matchings):
-	for key in matchings:
-		if matchings[key] >= 0.1:
-			del matchings[key]
+	for key in matchings.keys():
+	    if matchings[key][1] >= 50:
+		del matchings[key]
 
 	return matchings
 
@@ -109,7 +115,7 @@ def frame_matching(previous_squares, current_squares):
 
 	# This double for loop finds distance between every square in domain and every square in codomain
 	for ps in domain:
-		ps_matching = [:]
+		ps_matching = {}
 		ps.name = str(ps_name)
 
 		# First match is the smallest distance (optimal) without considering overlap
@@ -136,7 +142,8 @@ def frame_matching(previous_squares, current_squares):
 	overlaps, overlap_target = check_for_overlap(final_matching)
 
 	while overlaps:
-		overlap_target_current_match = final_matching[overlap_target]
+                for tester in final_matching:
+		    overlap_target_current_match = final_matching[overlap_target][0]
 		del matching_distances[overlap_target][overlap_target_current_match]
 
 		target_distance_dictionary = matching_distances[overlap_target]
@@ -146,52 +153,51 @@ def frame_matching(previous_squares, current_squares):
 			if next_best == None or next_best[1] > target_distance_dictionary[key]:
 				next_best = (key, target_distance_dictionary[key])
 
-		if next_best = None:
+		if next_best == None:
 			del final_matching[overlap_target]
 		else:
 			final_matching[overlap_target] = next_best
 
-		overlap, overlap_target = check_for_overlap(final_matching)
+		overlaps, overlap_target = check_for_overlap(final_matching)
+
 
 	return final_matching, flipped
 
 
 def check_for_overlap(matching_dictionary):
-	overlaps = None
 	overlap_target = None
 
-	for i in Range(len(matching_dictionary)):
-		for j in Range(i+1, len(matching_dictionary)):
-			if matching_dictionary[i][0] == matching_dictionary[j][0]:
-				conflict_1 = matching_dictionary[i]
-				conflict_2 = matching_dictionary[j]
+        for key_1 in matching_dictionary:
+            for key_2 in matching_dictionary:
+                if not key_1 == key_2 and matching_dictionary[key_1][0] == matching_dictionary[key_2][0]:
+                    conflict_1 = matching_dictionary[key_1]
+                    conflict_2 = matching_dictionary[key_2]
 
-				overlaps = True
-				if conflict_1[1] > conflict_2[1]:
-					overlap_target = conflict_1[0]
-				else:
-					overlap_target = conflict_2[0]
 
-	if overlaps == None:
-		overlaps = False
+                    if conflict_1[1] > conflict_2[1]:
+                        overlap_target = key_1
+                    else:
+                        overlap_target = key_2
 
-	return overlaps, overlap_target
+                    return True, overlap_target
+                        
+	return False, None
 
 # return vector distance between centers of squares
 def vec_distance(square_1, square_2):
 	corner_1 = get_bottom_left_corner(square_1)
 	corner_2 = get_bottom_left_corner(square_2)
 
-	return corner_2[0] - corner_1[0], corner_2[1] - corner_1[1]
+	return corner_2 - corner_1
 
 def get_bottom_left_corner(square):
 	lowest_corner = None
 
 	for corner in square.corners:
-		if lowest_corner == None or corner[1] < lowest_corner[1]:
+		if type(lowest_corner) is not np.ndarray or corner[0][1] < lowest_corner[0][1]:
 			lowest_corner = corner
-		elif corner[1] == lowest_corner[1]:
-			if corner[0] < lowest_corner[0]:
+		elif corner[0][1] == lowest_corner[0][1]:
+			if corner[0][0] < lowest_corner[0][0]:
 				lowest_corner = corner
 
 	return lowest_corner
